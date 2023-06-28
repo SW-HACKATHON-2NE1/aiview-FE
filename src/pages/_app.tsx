76f82fb69ecb71794ea81956ea5a25f092d7b4cc
Sharlotte <aaa9810321@gmail.com>
2023-06-29 03:13:14 +0900

    feat: add Pretendard font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import { ThemeProvider } from "@emotion/react";
 import type { AppProps } from "next/app";
 import { theme } from "@/core/theme";
 
+import "@public/global.css";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
