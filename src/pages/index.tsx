e78be4392b1394ec7d3674a3aa2e4492fccf9b48
johyewon <johyewon4601@gmail.com>
2023-06-29 18:23:12 +0900

    feat: add main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <>Hello world!</>;
+  return (
+    <main>
+      <div style={{ height: "23px"}}/>
+      <div style={{ height: "100%", width: "100%", display: "inline-flex", flexDirection: "column", justifyContent: "flex-start", alignItems: "center" }}>
+          <div style={{color: '#1A1A1A', fontSize: 28, fontFamily: 'Pretendard', fontWeight: '700', lineHeight: "44px", wordWrap: 'break-word'}}>AI로 실전처럼 면접을 연습하고 <br/>자신의 역량을 파악해보세요</div>
+          <div style={{height: "56px"}}/>
+          <button style={{width: '217px', height: '56px', padding: 24, background: '#497AF8', borderRadius: 10, justifyContent: 'center', alignItems: 'center', gap: 8, display: 'inline-flex', color: "white"}}>면접 시작하기</button>
+      </div>
+    </main>
+  );
 }
