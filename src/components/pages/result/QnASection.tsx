ccf46b4bbbb783bd85400ce32a7cf4f03b326b75
Sharlotte <aaa9810321@gmail.com>
2023-06-30 01:04:23 +0900

    feat: report 페이지 api 연동 완료

--- src/components/pages/result/QnASection.tsx
+++ src/components/pages/result/QnASection.tsx
@@ -1,88 +1,30 @@
-import Button from "@/components/Button";
 import CollapseBar from "@/components/Collapse";
 import Tabs from "@/components/Tabs";
-import styled from "@emotion/styled";
+import S from "./QnASection.styled";
 import { useState } from "react";
-
-const QnAContainer = styled("div")(({ theme }) => ({
-  marginTop: "32px",
-  "& h3": {
-    color: theme.color.G500,
-    fontSize: "18px",
-    fontWeight: 500,
-  },
-  "& p": {
-    color: theme.color.G800,
-    fontSize: "20px",
-    lineHeight: "44px",
-  },
-}));
-
-const SpeakSubSection = styled("section")(({ theme }) => ({
-  padding: "40px 24px",
-  display: "flex",
-  flexDirection: "column",
-  width: "690px",
-  gap: "56px",
-  borderRadius: "4px",
-  border: `1px solid ${theme.color.G200}`,
-}));
-
-const PointContainer = styled("div")(({ theme }) => ({
-  fontSize: "24px",
-  fontWeight: 700,
-  display: "flex",
-  gap: "24px",
-
-  "& span:nth-of-type(1)": {
-    color: theme.color.G900,
-  },
-  "& span:nth-of-type(2)": {
-    color: theme.color.blue,
-  },
-}));
-
-const StyledCollapseBar = styled(CollapseBar)(({ theme }) => ({
-  borderRadius: "4px",
-  padding: "32px 24px",
-  marginTop: "16px",
-  border: "1px solid #ccc",
-  width: "690px",
-  "& > div:nth-of-type(1)": {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  "& > div:nth-of-type(2)": {
-    padding: "32px 24px",
-    display: "flex",
-    flexDirection: "column",
-    gap: "40px",
-    "& > div:nth-of-type(2) > h3": {
-      color: theme.color.blue,
-    },
-  },
-}));
-
-const ReplyButton = styled(Button)({
-  marginTop: "56px",
-  width: "738px",
-});
 
 interface QnASectionProps {
   qnaArr: Array<{
     question: string;
-    answer: string;
-    speakPoint: number;
-    answerPoint: number;
+    transcription: string;
+    score: number;
     feedback: string;
-    correctAnswer: string;
+    bestAnswer: string;
+    questionId: number;
+    pronunciationScore: number;
+    url: string;
   }>;
 }
 function QnASection({ qnaArr }: QnASectionProps) {
   const [currentTabIndex, setCurrentTabIndex] = useState(0);
-  const { question, answer, speakPoint, answerPoint, feedback, correctAnswer } =
-    qnaArr[currentTabIndex];
+  const {
+    question,
+    transcription,
+    pronunciationScore,
+    score,
+    feedback,
+    bestAnswer,
+  } = qnaArr[currentTabIndex];
 
   return (
     <section>
@@ -91,28 +33,28 @@ function QnASection({ qnaArr }: QnASectionProps) {
         onTabChanged={(v) => setCurrentTabIndex(+v[2] - 1)}
         items={qnaArr.map((_, i) => `질문${i + 1}`)}
       />
-      <QnAContainer>
-        <SpeakSubSection>
+      <S.QnAContainer>
+        <S.SpeakSubSection>
           <div>
             <h3>AI의 질문</h3>
             <p>{question}</p>
           </div>
           <div>
             <h3>당신의 답변</h3>
-            <p>{answer}</p>
+            <p>{transcription}</p>
           </div>
-          <PointContainer>
+          <S.PointContainer>
             <span>발음 평가 점수</span>
-            <span>{speakPoint.toFixed(2).padStart(2, "0")}점</span>
-          </PointContainer>
-        </SpeakSubSection>
+            <span>{pronunciationScore.toFixed(2).padStart(2, "0")}점</span>
+          </S.PointContainer>
+        </S.SpeakSubSection>
 
-        <StyledCollapseBar>
+        <S.StyledCollapseBar>
           <div>
-            <PointContainer>
+            <S.PointContainer>
               <span>질문에 대한 적절성 평가</span>
-              <span>{answerPoint.toFixed(0).padStart(2, "0")}점</span>
-            </PointContainer>
+              <span>{score.toFixed(0).padStart(2, "0")}점</span>
+            </S.PointContainer>
 
             <CollapseBar.Button />
           </div>
@@ -124,12 +66,12 @@ function QnASection({ qnaArr }: QnASectionProps) {
             </div>
             <div>
               <h3>모범 답안</h3>
-              <p>{correctAnswer}</p>
+              <p>{bestAnswer}</p>
             </div>
           </CollapseBar.Body>
-        </StyledCollapseBar>
-        <ReplyButton>나의 녹화 다시 보기</ReplyButton>
-      </QnAContainer>
+        </S.StyledCollapseBar>
+        <S.ReplyButton>나의 녹화 다시 보기</S.ReplyButton>
+      </S.QnAContainer>
     </section>
   );
 }
--- src/pages/result.tsx
+++ src/pages/result.tsx
@@ -1,7 +1,9 @@
 import RadarChart from "@/components/RadarChart";
 import WordSpread from "@/components/WordsSpread";
 import QnASection from "@/components/pages/result/QnASection";
+import useSWR from "swr";
 import styled from "@emotion/styled";
+import CSR from "@/components/CSR";
 
 const VerticalDivider = styled("div")(({ theme }) => ({
   width: "1px",
@@ -24,7 +26,7 @@ const PageContainer = styled("div")({
   },
 });
 
-const PageBody = styled("div")({
+const PageBodyContainer = styled("div")({
   display: "flex",
   width: "100vw",
   justifyContent: "center",
@@ -35,83 +37,62 @@ const PageBody = styled("div")({
   },
 });
 
-const defaultWords = [
-  {
-    text: "told",
-    value: 64,
-  },
-  {
-    text: "mistake",
-    value: 11,
-  },
-  {
-    text: "thought",
-    value: 16,
-  },
-  {
-    text: "bad",
-    value: 17,
-  },
-  {
-    text: "tolaaad",
-    value: 264,
-  },
-  {
-    text: "miaaaastake",
-    value: 131,
-  },
-  {
-    text: "thaaaought",
-    value: 56,
-  },
-  {
-    text: "baaaad",
-    value: 77,
-  },
-];
-const defaultNumbers = {
-  scoreAL: 1,
-  scoreDB: 0.3,
-  scoreDS: 0.12,
-  scoreIS: 0.3,
-  scoreNT: 0.5,
-  scoreOS: 0.9,
-};
-const defaultQnAs = [
-  {
-    question: "TCP, UDP에 대해서 아는대로 설명해보세요.",
-    answer:
-      "페이징은 메모리를 동일한 크기의 페이지로 분할하여 물리 메모리의 프레임에 매핑하는 메모리 관리 기법입니다. 페이지 테이블을 사용하여 가상 주소를 물리 주소로 변환하며, 이를 통해 메모리 단편화를 줄이고, 가상 메모리 사용을 가능하게 합니다. 이 기법은 메모리를 효율적으로 사용하고 보호하는 데 도움이 되지만, 관리 오버헤드가 발생할 수 있습니다.",
-    speakPoint: 0,
-    answerPoint: 0,
-    feedback:
-      "답변에는 TCP와 UDP에 대한 설명이 혼동되고 잘못 표현되어있습니다. 실제로 TCP는 연결 지향형 프로토콜로, 데이터 전송 전에 3-way handshake를 통해 연결을 설정합니다. TCP는 데이터의 순서보장과 신뢰성을 제공합니다. 반대로, UDP는 비연결형 프로토콜로, 연결 설정 없이 데이터를 전송합니다. UDP는 빠른 전송을 제공하지만, 신뢰성이나 순서 보장은 하지 않습니다. 답변을수정하여 TCP와 UDP의 특징을 정확하게 반영하는 것이 중요합니다.",
-    correctAnswer:
-      "TCP(Transmission Control Protocol)는 연결 지향형 프로토콜로, 통신을 시작하기 전에 두 시스템 간에 연결을 설정합니다. 이는 3-way handshake 과정을 통해 이루어집니다. TCP는 데이터의 순서 보장, 신뢰성, 혼잡 제어 및 흐름 제어를 제공합니다. 이로 인해 높은 신뢰성이 필요한 애플리케이션에서 주로 사용됩니다. 반면, UDP(User Datagram Protocol)는 비연결형 프로토콜로, 데이터를 전송하기 전에 연결을 설정하지 않습니다. UDP는 낮은 지연 시간과 빠른 전송을 제공하지만, 데이터의 순서 보장이나 신뢰성은 제공하지 않습니다. 이로 인해 실시간 스트리밍, 게임, VoIP 등 빠른 데이터 전송이 중요한 경우에 적합합니다.",
-  },
-];
+function PageBody() {
+  const data = useSWR<ReportAPIResponse>([
+    "http://54.180.14.177/report",
+    typeof localStorage !== undefined ? localStorage.getItem("token") : "",
+  ]);
+
+  if (data.isLoading || !data.data) return <>loading...</>;
+  return (
+    <PageBodyContainer>
+      <section>
+        <div>
+          <h2>단어 빈도</h2>
+          <div>
+            <WordSpread
+              {...{
+                angry: data.data.angry,
+                calm: data.data.calm,
+                confused: data.data.confused,
+                disgusted: data.data.disgusted,
+                fear: data.data.fear,
+                happy: data.data.happy,
+                sad: data.data.sad,
+                surprised: data.data.surprised,
+              }}
+            />
+          </div>
+        </div>
+        <div>
+          <h2>나의 개발 역량 다이어그램</h2>
+          <div>
+            <RadarChart
+              numbers={{
+                scoreAL: data.data.scoreAL,
+                scoreDB: data.data.scoreDB,
+                scoreDS: data.data.scoreDS,
+                scoreIS: data.data.scoreIS,
+                scoreNT: data.data.scoreNT,
+                scoreOS: data.data.scoreOS,
+              }}
+            />
+          </div>
+        </div>
+      </section>
+      <VerticalDivider />
+      <QnASection qnaArr={data.data.reports} />
+    </PageBodyContainer>
+  );
+}
+
 export default function ResultPage() {
   return (
     <PageContainer>
       <h1>AI 면접 결과 리포트</h1>
-      <PageBody>
-        <section>
-          <div>
-            <h2>단어 빈도</h2>
-            <div>
-              <WordSpread words={defaultWords} />
-            </div>
-          </div>
-          <div>
-            <h2>나의 개발 역량 다이어그램</h2>
-            <div>
-              <RadarChart numbers={defaultNumbers} />
-            </div>
-          </div>
-        </section>
-        <VerticalDivider />
-        <QnASection qnaArr={defaultQnAs} />
-      </PageBody>
+      <CSR>
+        <PageBody />
+      </CSR>
     </PageContainer>
   );
 }
